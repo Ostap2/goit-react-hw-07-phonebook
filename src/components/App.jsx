d2b66699acba32b9f71f549a41b60403287ce09d
Ostap <Ostapkriven13@gmail.com>
2023-10-23 17:53:08 +0300

    і
    і

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import { useSelector } from 'react-redux';
+import ContactForm from 'components/Contact/ContactForm/ContactForm';
+import ContactList from 'components/Contact/ContactItem/ContactList';
+import Filter from 'components/Contact/Filter/Filter';
+import { Container } from './App.styled';
+
+export default function App() {
+  const { contacts } = useSelector((state) => state.contacts);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      {contacts.length === 0 ? (
+        <h3>No contacts</h3>
+      ) : (
+        <>
+          <Filter />
+          <ContactList />
+        </>
+      )}
+    </Container>
   );
-};
+}
